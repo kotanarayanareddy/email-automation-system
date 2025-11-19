5d69ffa1cd9295e6ee15fc8dd87b9236d5610d19
Kota Lakshmi Narayana Reddy <lakshminarayanareddykota5@gmail.com>
2025-11-20 02:31:15 +0530

    Update job function for GitHub Actions trigger

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,3 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
 from send_email import send_emails
 from datetime import datetime
 
@@ -6,21 +5,7 @@ def log_message(message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"[{timestamp}] {message}")
 
-def job():
-    log_message("Scheduler triggered")
-    send_emails()
-
 if __name__ == "__main__":
-    scheduler = BlockingScheduler()
-
-    # scheduler.add_job(job, 'cron', hour=00, minute=44)
-    # scheduler.add_job(job, 'cron', hour=23, minute=52)
-
-
-    scheduler.add_job(job, 'cron', day=1, hour=9, minute=0)
-
-    log_message("Scheduler started. Waiting for jobs...")
-    try:
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        log_message("Scheduler stopped.")
+    log_message("Scheduler triggered by GitHub Actions")
+    send_emails()
+    log_message("Email sending completed")
